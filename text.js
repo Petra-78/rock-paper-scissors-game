124d20cfe495e7df0e9645f152d10e8cb2398deb
Petra-78 <partos.petra.0305@gmail.com>
2025-04-07 15:12:16 +0200

    js updated play rounds

--- text.js
+++ text.js
@@ -1,5 +1,48 @@
+let humanScore = 0;
+let compScore = 0;
 
 function getCompRandom () {
     let n = Math.floor(Math.random() * 3)
-    return ("the number is " + n);
+        if (n === 1) {
+        return ("rock");
+    }   else if (n === 2) {
+        return ("paper");
+    }   else {
+        return ("scissors");
+    }
 }
+
+function getHumanChoice() {
+    let guess = prompt("Write your guess");
+    return guess;
+}
+
+function playRound(humanChoice, compChoice) {
+    
+    if (humanChoice.toLowerCase() === "rock" && compChoice === "paper") {
+        console.log("You lost! Paper beats rock.")
+}   else if (humanChoice.toLowerCase() === "rock" && compChoice === "scissors") {
+        console.log("Congrats, you won!")
+        humanScore++ 
+}   else if (humanChoice.toLowerCase() === "paper" && compChoice === "rock") {
+        console.log("Congrats, you won!")
+        humanScore++ 
+}   else if (humanChoice.toLowerCase() === "paper" && compChoice === "scissors") {
+        console.log("You lost! Scissors beats paper.")
+        compScore++ 
+}   else if (humanChoice.toLowerCase() === "scissors" && compChoice === "paper") {
+        console.log("Congrats, you won!")
+        humanScore++
+}   else if (humanChoice.toLowerCase() === "scissors" && compChoice === "rock") {
+        console.log("You lost! Rock beats scissors.")
+        compScore++
+}   else if (humanChoice.toLowerCase() === compChoice) {
+        console.log("It's a tie!")
+}
+}
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getCompRandom();
+  
+  playRound(humanSelection, computerSelection);
+  
