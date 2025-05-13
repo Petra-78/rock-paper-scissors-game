3cf4e8740cae1ddb7c02db5ae43401794a5bfa63
Petra-78 <partos.petra.0305@gmail.com>
2025-05-13 13:10:40 +0200

    5 points system + popup added. Game logic complete

--- text.js
+++ text.js
@@ -1,10 +1,14 @@
 let humanScore = 0;
 let compScore = 0;
 
-let humanSc = document.querySelector(".score-human");
-let compSc = document.querySelector(".score-comp");
+const humanSc = document.querySelector(".score-human");
+const compSc = document.querySelector(".score-comp");
+
 const roundsTitle = document.querySelector(".rounds-title");
 const roundsPara = document.querySelector(".rounds-para");
+
+const popup = document.querySelector(".popup");
+const popupText = document.querySelector(".popup-text")
 
 function getCompChoice () {
     let n = Math.floor(Math.random() * 3)
@@ -18,8 +22,16 @@ function getCompChoice () {
 }
 
 function playRound(humanChoice, compChoice) {
+
+    if (humanScore === 5) {
+        popupText.textContent = "You won the game!";
+        popup.style.display = "flex";
+}   else if (compScore === 5) {
+        popupText.textContent = "You lost the game!";
+        popup.style.display = "flex";
+}
     
-    if (humanChoice.toLowerCase() === "rock" && compChoice === "paper") {
+    else if (humanChoice.toLowerCase() === "rock" && compChoice === "paper") {
         roundsTitle.textContent = ("You lost!");
         roundsPara.textContent = ("Paper beats rock.");
         compSc.textContent = ("score: " + ++compScore);
@@ -46,8 +58,10 @@ function playRound(humanChoice, compChoice) {
 }   else if (humanChoice.toLowerCase() === compChoice) {
         roundsTitle.textContent = ("It's a tie!");
         roundsPara.textContent = (" ");
+}   
 }
-}
+
+
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
@@ -75,6 +89,20 @@ scissorsButton.addEventListener("click", function() {
     player2Pic.src = "./images/" + computer + ".png";
 });
 
+
+const closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", function() {
+    popup.style.display = "none";
+    roundsTitle.textContent = "Choose your weapon!";
+    roundsPara.textContent = "You have 5 rounds to win the game.";
+    player1Pic.src = "./images/questionmark.png";
+    player2Pic.src = "./images/questionmark.png";
+    humanSc.textContent = "score: 0";
+    compSc.textContent = "score: 0";
+    humanScore = 0;
+    compScore = 0;
+})
 
 
 
